3c49f3e12ad35eefbbec113256986a1205c6bd7f
glinren <example@example.com>
2024-01-28 00:03:56 +0100

    Now I can get questions from the database.^

--- database/init_scripts/EA0_enter_test_question.sh
+++ database/init_scripts/EA0_enter_test_question.sh
@@ -3,7 +3,7 @@ set -e
 psql -v ON_ERROR_STOP=1 -U mathlingo -d mathlingo  <<-EOSQL
 
 INSERT INTO question(type, content) VALUES ('SINGLE_CHOICE', '{"type_":"single_choice",
-     "text": "Was ist die bedeutung von  $\\\\mathbb{N}$?",
+     "text": "Was ist die bedeutung von  $\\\\mathbb{N}$ ?",
      "correctChoice":2, 
      "answers":["Alle ganzen Zahlen","$\\\\{0,1,2...\\\\}$","$\\\\{1,2,3,4...\\\\}$"]}'::json);
 
--- website/app/app/lection/actions.ts
+++ website/app/app/lection/actions.ts
@@ -4,6 +4,9 @@ import {QuestionT,
 	SingleChoiceQuestionT,
 	isSingleChoiceQuestionT
        } from './QuestionTypes' 
+import {Client} from 'pg'
+
+
 
 const questions: QuestionT[]=[
     {type_:"single_choice",
@@ -11,7 +14,23 @@ const questions: QuestionT[]=[
      correctChoice:2, 
      answers:["Alle ganzen Zahlen","$\\{0,1,2...\\}$","$\\{1,2,3,4...\\}$"]}]
 
-
+function logError(err){
+    console.log("Error:"+err)
+    return null;
+}
 export async function fetchQuestions(){
-    return questions;
+    console.log(process.env.MATHLINGO_PASSWORD)
+    const client = new Client({
+	user: 'mathlingo',
+	host: 'mathlingo_database',
+	database: 'mathlingo',
+	password: process.env.MATHLINGO_PASSWORD,
+	port:5432
+    })
+    await client.connect();
+    const res = await client.query(
+	'SELECT content FROM question;');
+    await client.end();
+    console.log(res.rows[0]["content"])
+    return [res.rows[0]["content"]];
 }
